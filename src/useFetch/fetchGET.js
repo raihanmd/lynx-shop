e993473755a8b8fef8aec8a09eec70acc8354881
raihanmd <mraihanf471@gmail.com>
2023-09-30 21:16:45 +0700

    bug in my usefetch hooks

--- src/useFetch/fetchGET.js
+++ src/useFetch/fetchGET.js
@@ -1,8 +1,6 @@
 export const fetchGET = async (url, options) => {
   try {
-    const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } });
-
-    return await res.json();
+    return await (await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } })).json();
   } catch (error) {
     throw error;
   }
--- src/useFetch/fetchPOST.js
+++ src/useFetch/fetchPOST.js
@@ -1,15 +1,15 @@
 export const fetchPOST = async (url, data, options) => {
   try {
-    const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, {
-      method: "POST",
-      headers: {
-        "API-Key": process.env.NEXT_PUBLIC_API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const json = await res.json();
-    return json;
+    return await (
+      await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, {
+        method: "POST",
+        headers: {
+          "API-Key": process.env.NEXT_PUBLIC_API_KEY,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+    ).json();
   } catch (error) {
     throw error;
   }
