1d49be9107d3dddab471fe8202cbed2e129bd49a
raihanmd <mraihanf471@gmail.com>
2023-09-30 21:45:13 +0700

    fixing

--- src/useFetch/fetchGET.js
+++ src/useFetch/fetchGET.js
@@ -1,8 +1,10 @@
 export const fetchGET = async (url, options) => {
   try {
-    const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } });
+    // const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } });
+    const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url);
 
-    return await res.json();
+    const json = await res.json();
+    return json;
   } catch (error) {
     throw error;
   }
