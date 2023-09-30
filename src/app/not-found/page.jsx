b06dbcfb930246be93527c30bd78aca7a1fb6d0e
raihanmd <mraihanf471@gmail.com>
2023-09-30 20:53:08 +0700

    added feature/not-found

---
+++ src/app/not-found/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Flex } from "@chakra-ui/react";
+
+export default function UserNotFound() {
+  return (
+    <Flex w={"full"} h={"85vh"} fontSize={"xl"} justify={"center"} align={"center"} fontWeight={"semibold"}>
+      404 | Halaman tidak ditemukan.
+    </Flex>
+  );
+}
