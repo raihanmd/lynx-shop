12d91faf3f81e08e7936051d75da313d06afde0d
raihanmd <mraihanf471@gmail.com>
2023-09-30 21:33:24 +0700

    still trying to fix this json parse bug

--- src/useFetch/fetchGET.js
+++ src/useFetch/fetchGET.js
@@ -1,7 +1,10 @@
 export const fetchGET = async (url, options) => {
   try {
-    return await (await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } })).json();
+    const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } });
+    const json = await res.json();
+    return json;
   } catch (error) {
+    console.log(error);
     throw error;
   }
 };
--- src/useFetch/fetchPOST.js
+++ src/useFetch/fetchPOST.js
@@ -1,16 +1,18 @@
 export const fetchPOST = async (url, data, options) => {
   try {
-    return await (
-      await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, {
-        method: "POST",
-        headers: {
-          "API-Key": process.env.NEXT_PUBLIC_API_KEY,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-    ).json();
+    const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, {
+      method: "POST",
+      headers: {
+        "API-Key": process.env.NEXT_PUBLIC_API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const json = await res.json();
+    return json;
   } catch (error) {
+    console.log(error);
     throw error;
   }
 };
