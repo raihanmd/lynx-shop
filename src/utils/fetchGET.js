afb0f03c829004d2a820a8ff2b55008326a41988
raihanmd <mraihanf471@gmail.com>
2023-10-01 21:44:51 +0700

    update

--- src/utils/fetchGET.js
+++ src/utils/fetchGET.js
@@ -3,6 +3,8 @@ export const fetchGET = async (url, options) => {
     // const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } });
     const res = await fetch(options?.component === "client" ? url : process.env.NEXT_PUBLIC_MAIN_URL + url, { next: { revalidate: 10 } });
 
+    if (res.headers.get("Content-Type") !== "application/json") return { message: "Error" };
+
     const json = await res.json();
     return json;
   } catch (error) {
